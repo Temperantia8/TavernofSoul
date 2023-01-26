1e830af5a7bdaff51286911d59d08d9efca9fd37
temperantia8 <temperantia.temp@gmail.com>
2023-01-26 20:47:17 +0700

    fix ies

--- parser_tidy/iesutil.py
+++ parser_tidy/iesutil.py
@@ -14,12 +14,12 @@ from DB import ToS_DB as constants
 def load(ies_name,c):
     ies_data = []
     #ies_path = os.path.join(constants.PATH_INPUT_DATA, "ies.ipf", ies_name)
-    ies_path = c.file_dict[ies_name.lower()]['path']
+    
 
-    if not os.path.exists(ies_path):
+    if not ies_name.lower() not in c.file_dict:
         logging.warn('Missing ies file: %s', ies_path)
         return []
-
+    ies_path = c.file_dict[ies_name.lower()]['path']
     with io.open(ies_path, 'r', encoding = "utf-8") as ies_file:
         ies_reader = csv.DictReader(ies_file, delimiter=',', quotechar='"')
 
